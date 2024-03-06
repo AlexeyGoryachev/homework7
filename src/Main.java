dfa9e4e5056dad27656344e2d2972de25f11e3c3
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-06 17:43:25 +0300

    task 2 is done.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Homework 7.");
-        System.out.println("");
+        System.out.println(" ");
         System.out.println("task 1.");
         int deposit = 15000;
         int total = 0;
@@ -12,6 +12,18 @@ public class Main {
             total = total + total/100;
             System.out.println("month " + month + " amount accumulated " + total);
         }
-
+        System.out.println(" ");
+        System.out.println("task 2.");
+        int up = 0;
+        while (up < 10) {
+            up++;
+            System.out.print(up + " ");
+        }
+        System.out.println(" ");
+        up = 10;
+        while (up >= 1) {
+            System.out.print(up + " ");
+            up--;
+        }
     }
 }
